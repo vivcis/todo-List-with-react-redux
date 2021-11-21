50c1de7c4b8fce84d6ec580ef02dc67573cf833a
unknown <ceciliaorji.co@gmail.com>
2021-11-21 19:18:49 +0100

    todo list built with react redux

---
+++ src/components/TodoInput.js
@@ -0,0 +1,27 @@
+import React, {useState} from "react"
+import "./TodoInput.css";
+
+const TodoInput = ({createTodo}) => {
+    const [task, setTask] = useState("");
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      createTodo(task);
+      setTask("");
+    };
+   return (
+       <form className= "TodoInput" onSubmit={handleSubmit}>
+        <input
+        type = "text"
+        placeholder = "add a new task"
+        id = "task"
+        name = "task"
+        value = {task}
+        onChange = {(e) => setTask(e.target.value)}
+        />
+        <button>Add Todo</button>
+       </form>
+   );
+};
+
+export default TodoInput;
